7dca24cc7a985ac7e23f64bf88ff18ca99a13818
merialea <merialea5@gmail.com>
2025-09-07 12:34:36 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,16 @@
+const divide = function(a,b) {
+return a / b;
+}
+let a = 2000;
+let b = 100;
+
+const square = (x) => {
+    return x*x;
+}
+let x = 2;
+
+const add = (c,d) => {
+    return c+d;
+}
+let c = 3;
+let d = 4;
